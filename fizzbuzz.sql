b67c873a8d61af38e129b4bf422d2631260ebf89
Daniel Jacobs <danielhunterjacobs@gmail.com>
2021-05-11 14:56:24 -0400

    Update fizzbuzz.sql

--- fizzbuzz.sql
+++ fizzbuzz.sql
@@ -1,16 +1,15 @@
+-- This works for latest PostgreSQL, SQLite, and MySQL
+-- However, you must either use CONCAT instead of pipes or un-comment the next line for MySQL
+--SET sql_mode = 'PIPES_AS_CONCAT';
 WITH RECURSIVE
-cnt(x) AS (
+cnt(i) AS (
   SELECT 1
   UNION ALL
-  SELECT x+1 FROM cnt
-),
-cnt_limit as (
-  SELECT * FROM cnt
-  LIMIT 100
+  SELECT i+1 FROM cnt
+  WHERE i < 100
 )
 SELECT
-    CASE WHEN output <> '' THEN output
-    ELSE CAST(x AS TEXT) END
+(CASE WHEN output <> '' THEN output ELSE CAST(i AS CHAR(3)) END) AS output
 FROM (
-    SELECT CASE WHEN x%3 = 0 THEN 'fizz' ELSE '' END || CASE WHEN x%5 = 0 THEN 'buzz' ELSE '' END AS output, x FROM cnt_limit
+    SELECT CASE WHEN i % 3 = 0 THEN 'Fizz' ELSE '' END || CASE WHEN i % 5 = 0 THEN 'Buzz' ELSE '' END AS output, i FROM cnt
 ) AS subquery;
