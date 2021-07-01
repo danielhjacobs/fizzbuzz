e49808578ba58bf132d852a78f85510fd55fde1a
Daniel Jacobs <danielhunterjacobs@gmail.com>
2021-07-01 14:33:45 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,11 +4,11 @@ dict = {
 }
 for i in range(1, 101):
   output = ""
-  # sorted only needed in Python version below 3.7
+  # sorted needed in Python version below 3.7
   # For Python 3.7+, insertion order is preserved:
   # "the insertion-order preservation nature of dict objects has been declared to be an official part of the Python language spec."
   # https://docs.python.org/3/whatsnew/3.7.html
-  for key in sorted(dict):
+  for key in dict:
     if i % key == 0:
       output += dict[key]
   if output == "":
