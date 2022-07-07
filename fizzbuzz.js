85827f1f0271ce885e9a5c1bbb706291303342a7
Daniel Jacobs <danielhunterjacobs@gmail.com>
2022-07-07 15:37:05 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,11 +3,9 @@ const dict = new Map([
   [3, "Fizz"],
   [5, "Buzz"],
 ]);
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; ++i) {
   let output = "";
-  dict.forEach(function(value, key) {
-    if ( i % key === 0) { output += value; }
-  });
-  if (output === "") { output = i; }
+  dict.forEach((value, key) => output += i % key === 0 ? value : "")
+  if (output === "") output = i;
   console.log(output);
 }
